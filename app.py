20d603f07c35a22eff9fd7828c3fc361661fcb88
tgx0409 <tinggx-wp24@student.tarc.edu.my>
2026-08-30 00:45:07 +0800

    Update app.py

--- app.py
+++ app.py
@@ -98,7 +98,7 @@ st.markdown(
         padding: 1.1rem 1rem;
         text-align: center;
         margin-bottom: 0.6rem;
-        height: 150px;          
+        height: 10px;          
         aspect-ratio: 1 / 1; 
         display: flex;
         flex-direction: column;
