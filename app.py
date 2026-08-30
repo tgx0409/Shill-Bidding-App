020e2b90dc44d28b1384a0f885f523a88189d973
tgx0409 <tinggx-wp24@student.tarc.edu.my>
2026-08-30 16:57:49 +0800

    Update app.py

--- app.py
+++ app.py
@@ -341,7 +341,7 @@ st.markdown(
     }}
     div[class*="st-key-card_gauge"] h3 {{
         text-align: center;
-        min-height: 780px;
+        min-height: 400px;
     }}
 
     /* ---- section banner ---- */
