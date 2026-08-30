60ce78f9e7634dae2628a8e649ed1aa787ae67c9
tgx0409 <tinggx-wp24@student.tarc.edu.my>
2026-08-30 15:19:10 +0800

    Update app.py

--- app.py
+++ app.py
@@ -258,8 +258,8 @@ st.markdown(
     div[class*="st-key-card_sliders"] .stButton {{
         display: flex;
         justify-content: flex-end;
-        margin-left: 5rem;
-        margin-right: 2rem;
+        margin-left: 6rem;
+        margin-right: 1rem;
         margin-top: 1.5rem;
     }}
     }}
