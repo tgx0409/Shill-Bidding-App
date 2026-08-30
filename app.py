1d561e05c95c72c17f23f0150ef543ee22164d29
tgx0409 <tinggx-wp24@student.tarc.edu.my>
2026-08-30 12:58:57 +0800

    Update app.py

--- app.py
+++ app.py
@@ -97,7 +97,7 @@ st.markdown(
         border-radius: 22px;
         padding: 0.7rem 0.6rem;
         margin-bottom: 0.6rem;
-        min-height: 130px;
+        min-height: 125px;
         display: flex;
         flex-direction: column;
         justify-content: center;
