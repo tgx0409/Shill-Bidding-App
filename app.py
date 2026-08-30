d4eedb17180e1bda52e2a61033bcf413796a3347
tgx0409 <tinggx-wp24@student.tarc.edu.my>
2026-08-30 13:25:38 +0800

    Update app.py

--- app.py
+++ app.py
@@ -431,7 +431,7 @@ elif page == "Explore the data":
     with col1:
         with card("card_class_balance"):
             st.markdown("**Class distribution**")
-            fig, ax = plt.subplots(figsize=(10, 10))
+            fig, ax = plt.subplots(figsize=(4, 4))
             df["Class"].value_counts().sort_index().plot(
                 kind="bar", ax=ax, color=["#FBDA0C", "#0057AD"]
             )
