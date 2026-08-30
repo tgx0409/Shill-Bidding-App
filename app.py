7b310fad40634269fec702d0f5f978f59343ce39
tgx0409 <tinggx-wp24@student.tarc.edu.my>
2026-08-30 17:15:19 +0800

    Update app.py

--- app.py
+++ app.py
@@ -325,7 +325,7 @@ st.markdown(
         justify-content: center;
         gap: 2.4rem;
         margin-top: 0.1rem;
-        margin-bottom: 0.5rem;
+        margin-bottom: 0.6rem;
     }}
     .risk-legend-item {{
         display: flex;
